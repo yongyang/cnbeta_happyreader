a199f53d5218d55dbe467712d6b793e2e33a9267
yyang <yyang@redhat.com>
2013-08-01 16:53:01 +0800

    delete loadmanager, refactor

---
+++ src/org/jandroid/cnbeta/async/RealtimeArticleListAsyncTask.java
@@ -0,0 +1,43 @@
+package org.jandroid.cnbeta.async;
+
+import org.jandroid.cnbeta.entity.Article;
+import org.jandroid.cnbeta.loader.ArticleListLoader;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:jfox.young@gmail.com">Young Yang</a>
+ */
+public abstract class RealtimeArticleListAsyncTask extends ProgressDialogAsyncTask<Object, Integer, AsyncResult> {
+
+    private ArticleListLoader.Type category;
+    private int page;
+
+    protected RealtimeArticleListAsyncTask(ArticleListLoader.Type category, int page) {
+        this.category = category;
+        this.page = page;
+    }
+
+    public ArticleListLoader.Type getCategory() {
+        return category;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    @Override
+    protected AsyncResult doInBackground(Object... params) {
+        try {
+            List<Article> articles = loadArticleList();
+            return AsyncResult.successResult(articles);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return AsyncResult.errorResult(e.toString());
+        }
+    }
+    
+    protected abstract List<Article> loadArticleList() throws Exception ;
+
+}
---
+++ src/org/jandroid/cnbeta/fragment/RealtimeArticleListFragment.java
@@ -0,0 +1,9 @@
+package org.jandroid.cnbeta.fragment;
+
+/**
+ * @author <a href="mailto:yyang@redhat.com">Yong Yang</a>
+ * @create 8/1/13 4:52 PM
+ */
+public class RealtimeArticleListFragment {
+
+}
