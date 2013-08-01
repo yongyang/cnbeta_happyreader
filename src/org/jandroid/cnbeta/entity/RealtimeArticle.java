1a53bf9ddeeaf7e0e1e74737d579d0624965dc65
yongyong <yy.young@gmail.com>
2013-08-01 14:17:25 +0800

    add RealtimeArticle

---
+++ src/org/jandroid/cnbeta/entity/RealtimeArticle.java
@@ -0,0 +1,11 @@
+package org.jandroid.cnbeta.entity;
+
+/**
+ * @author <a href="mailto:jfox.young@gmail.com">Young Yang</a>
+ */
+public class RealtimeArticle {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/org/jandroid/cnbeta/loader/AbstractLoader.java
+++ src/org/jandroid/cnbeta/loader/AbstractLoader.java
@@ -3,16 +3,26 @@ package org.jandroid.cnbeta.loader;
 /**
  * @author <a href="mailto:jfox.young@gmail.com">Young Yang</a>
  */
-public abstract class LoaderTask<T> {
+public abstract class AbstractLoader<T> {
 
     private String taskName;
+
+    private T loadedObject;
 
     public String getTaskName() {
         return taskName;
     }
 
+    public T getLoadedObject() {
+        return loadedObject;
+    }
+
     public void setTaskName(String taskName) {
         this.taskName = taskName;
+    }
+
+    public final void load(){
+        //TODO:
     }
 
     public abstract T fromHttp() throws Exception;
