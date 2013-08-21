651e1844a4d7d1ee8dcd7ea9e012b9a0549a8cb6
yyang <yyang@redhat.com>
2013-08-21 15:17:59 +0800

    Update content layout and Fragment

--- src/org/jandroid/cnbeta/async/ArticleContentAsyncTask.java
+++ src/org/jandroid/cnbeta/async/ArticleContentAsyncTask.java
@@ -42,7 +42,7 @@ public abstract class ArticleContentAsyncTask extends ProgressDialogAsyncTask<Ob
             return articles;
         }
 */
-        return new Content();
+        return null;
 
     }
 
