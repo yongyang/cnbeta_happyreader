215a871a838c88a3255d4bd5cec596b57cbbf966
yongyong <yy.young@gmail.com>
2013-08-23 11:09:37 +0800

    class method from WebView JS

--- src/org/jandroid/cnbeta/fragment/ArticleContentFragment.java
+++ src/org/jandroid/cnbeta/fragment/ArticleContentFragment.java
@@ -42,12 +42,11 @@ public class ArticleContentFragment extends Fragment {
     private WebView contentWebView;
 
 
-    
     //TODO: sn 从 article.html 页面中取, sid和sn必须要匹配
     //TODO: 可能还需要拿到 TOKEN: 'ae14639495b0ae0c848e2adaefc0f31db276167a',
     // http://www.cnbeta.com/cmt?jsoncallback=okcb91797948&op=info&page=1&sid=247973&sn=88747
     // okcb91797948({"status":"success","result":"Y25iZXRheyJjbW50ZGljdCI6W10sImhvdGxpc3QiOltdLCJjbW50bGlzdCI6W10sImNvbW1lbnRfbnVtIjoiMjEiLCJqb2luX251bSI6MCwidG9rZW4iOiIyYzM3MzBmY2I3OTE4N2IxNDU2NmQwMzFiOTc2MGQ1YzIxMGRlMWVhIiwidmlld19udW0iOjQzOTAsInBhZ2UiOjEsInNpZCI6MjQ3OTczLCJ1IjpbXX0="})
-   // base64 decode 之后
+    // base64 decode 之后
     // cnbeta{"cmntdict":[],"hotlist":[],"cmntlist":[],"join_num":0,"comment_num":0,"token":"1555e16583e8fd4f30b360f78f9d79cf0b5288f4","view_num":169,"page":1,"sid":249281,"u":[]}
     // cnbeta{"cmntdict":[],"hotlist":[],"cmntlist":[{"tid":"7764550","pid":"0","sid":"249281","parent":"","thread":""}],"cmntstore":{"7764550":{"tid":"7764550","pid":"0","sid":"249281","date":"2013-08-20 15:14:01","name":"\u533f\u540d\u4eba\u58eb","host_name":"\u6d59\u6c5f\u7701\u7ecd\u5174\u5e02","comment":"\u8def\u8fc7...","score":"0","reason":1,"userid":"0","icon":""}},"comment_num":"2","join_num":"1","token":"38a5032136b4e3097c28670a7cdd0c7fad2d1c62","view_num":370,"page":1,"sid":249281,"u":[]}
     
@@ -63,19 +62,19 @@ http://static.cnbetacdn.com/assets/js/utils/article.js?v=20130808
 
     //TODO: 支持 视频？？？
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)	{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.content_article, null);
-        titleTextView = (TextView)root.findViewById(R.id.tv_articleTitle);
-        titleTextView.setText(((ContentActivity)getActivity()).getArticleTitle());
+        titleTextView = (TextView) root.findViewById(R.id.tv_articleTitle);
+        titleTextView.setText(((ContentActivity) getActivity()).getArticleTitle());
         titleTextView.setSelected(true);
-        timeTextView = (TextView)root.findViewById(R.id.tv_date);
-        viewNumTextView = (TextView)root.findViewById(R.id.tv_viewNum);
-        commentNumTextView = (TextView)root.findViewById(R.id.tv_commentNum);
-        whereTextView = (TextView)root.findViewById(R.id.tv_where);
+        timeTextView = (TextView) root.findViewById(R.id.tv_date);
+        viewNumTextView = (TextView) root.findViewById(R.id.tv_viewNum);
+        commentNumTextView = (TextView) root.findViewById(R.id.tv_commentNum);
+        whereTextView = (TextView) root.findViewById(R.id.tv_where);
 
-        ratingBar = (RatingBar)root.findViewById(R.id.rating);
-        contentWebView = (WebView)root.findViewById(R.id.wv_articleContent);
+        ratingBar = (RatingBar) root.findViewById(R.id.rating);
+        contentWebView = (WebView) root.findViewById(R.id.wv_articleContent);
         // work weird
 //        contentWebView.setBackgroundColor(R.color.cnBeta_bg_introduction);
         contentWebView.getSettings().setJavaScriptEnabled(true);
@@ -95,13 +94,13 @@ http://static.cnbetacdn.com/assets/js/utils/article.js?v=20130808
         contentWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(false);
         contentWebView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
 
-        contentWebView.addJavascriptInterface(new Object(){
+        contentWebView.addJavascriptInterface(new Object() {
             @JavascriptInterface
-    		public void openImage(String img) {
-    			Intent intent = new Intent();
-    			intent.putExtra("image", img);
-    //			intent.setClass(context, ShowWebImageActivity.class);
-    //			context.startActivity(intent);
+            public void openImage(String img) {
+                Intent intent = new Intent();
+                intent.putExtra("image", img);
+                //			intent.setClass(context, ShowWebImageActivity.class);
+                //			context.startActivity(intent);
                 Toast.makeText(getActivity(), "点击了图片: " + img, Toast.LENGTH_SHORT).show();
                 //TODO: 新开一个 Transparent Activity, 使用 WebView 打开大图
             }
@@ -137,18 +136,19 @@ http://static.cnbetacdn.com/assets/js/utils/article.js?v=20130808
             }
         });
 
-        contentWebView.setWebChromeClient(new WebChromeClient(){
+        // 处理消息 和 Alert
+        contentWebView.setWebChromeClient(new WebChromeClient() {
             @Override
-             public boolean onConsoleMessage(ConsoleMessage cm) {
-                 log.d(cm.message() + " -- From line " + cm.lineNumber() + " of " + cm.sourceId() );
-                 return true;
-             }
+            public boolean onConsoleMessage(ConsoleMessage cm) {
+                log.d(cm.message() + " -- From line " + cm.lineNumber() + " of " + cm.sourceId());
+                return true;
+            }
 
-             @Override
-             public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-                 Toast.makeText(getActivity(), "JSAlert: \n" + message, Toast.LENGTH_SHORT).show();
-                 return true;
-             }
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                Toast.makeText(getActivity(), "JSAlert: \n" + message, Toast.LENGTH_SHORT).show();
+                return true;
+            }
 
         });
 
@@ -158,7 +158,7 @@ http://static.cnbetacdn.com/assets/js/utils/article.js?v=20130808
         //picView.getSettings().setBuiltInZoomControls(true);
         //picView.getSettings().setUseWideViewPort(true);
         return root;
-	}
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -178,7 +178,7 @@ http://static.cnbetacdn.com/assets/js/utils/article.js?v=20130808
         titleTextView.setText(content.getTitle());
         titleTextView.setSelected(true);
         timeTextView.setText(content.getTime());
-        viewNumTextView.setText(""+content.getViewNum());
+        viewNumTextView.setText("" + content.getViewNum());
         commentNumTextView.setText("" + content.getCommentNum());
         whereTextView.setText(content.getWhere());
         contentWebView.loadDataWithBaseURL("", content.getContent(), "text/html", "UTF-8", "");
@@ -187,30 +187,30 @@ http://static.cnbetacdn.com/assets/js/utils/article.js?v=20130808
     public void updateImage(String id, String imgSrc) {
         // 在android代码中调用javaScript方法
         contentWebView.loadUrl("javascript:(function(){" +
-      		"var objs = document.getElementsByTagName(\"img\"); " +
-      				"for(var i=0;i<objs.length;i++)  " +
-      		"{"
-      				+ "    objs[i].src='" + imgSrc + "';" +
-      		"}" +
-      		"})()");
+                "var objs = document.getElementsByTagName(\"img\"); " +
+                "for(var i=0;i<objs.length;i++)  " +
+                "{"
+                + "    objs[i].src='" + imgSrc + "';" +
+                "}" +
+                "})()");
         //TODO: WebView从APK中加载Assets目录中的内容，是否需要在 asset 中存放一张默认图片
         contentWebView.loadUrl("file:///android_asset/personaldata.html");
     }
 
-    	// 注入js函数监听
-	private void addImageClickListener() {
-		// 给所有img添加onclick函数，点击时打开大图
+    // 注入js函数监听
+    private void addImageClickListener() {
+        // 给所有img添加onclick函数，点击时打开大图
         //TODO: 要排除掉 topic 图片
-		contentWebView.loadUrl("javascript:(function(){" +
-		"var objs = document.getElementsByTagName(\"img\"); " +
-				"for(var i=0;i<objs.length;i++)  " +
-		"{"
-				+ "    objs[i].onclick=function()  " +
-		"    {  "
-				+ "window.JS.openImage(this.src);  " +
-		"    }  " +
-		"}" +
-		"})()");
-	}
+        contentWebView.loadUrl("javascript:(function(){" +
+                "var objs = document.getElementsByTagName(\"img\"); " +
+                "for(var i=0;i<objs.length;i++)  " +
+                "{"
+                + "    objs[i].onclick=function()  " +
+                "    {  "
+                + "window.JS.openImage(this.src);  " +
+                "    }  " +
+                "}" +
+                "})()");
+    }
 
 }
