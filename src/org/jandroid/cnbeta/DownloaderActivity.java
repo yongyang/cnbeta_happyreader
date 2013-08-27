900efc3fdb684aafb5779702fd6d17c9381aa20b
yyang <yyang@redhat.com>
2013-08-27 15:04:25 +0800

    DownloadActivity update

--- src/org/jandroid/cnbeta/DownloaderActivity.java
+++ src/org/jandroid/cnbeta/DownloaderActivity.java
@@ -13,71 +13,9 @@ import org.jandroid.common.Logger;
  */
 public class DownloaderActivity extends BaseActivity {
 
-    protected Logger logger = Logger.newLogger(this.getClass());
 
-   	protected AlertDialog mAlertDialog;
-   	protected AsyncTask mRunningTask;
-
-   	/******************************** 【Activity LifeCycle For Debug】 *******************************************/
-   	@Override
-   	protected void onCreate(Bundle savedInstanceState) {
-        logger.d("onCreate() invoked!!");
-   		super.onCreate(savedInstanceState);
-   		requestWindowFeature(Window.FEATURE_NO_TITLE);
-   	}
-
-   	@Override
-   	protected void onStart() {
-   		logger.d(" onStart() invoked!!");
-   		super.onStart();
-   	}
-
-   	@Override
-   	protected void onRestart() {
-   		logger.d(" onRestart() invoked!!");
-   		super.onRestart();
-   	}
-
-   	@Override
-   	protected void onResume() {
-   		logger.d(" onResume() invoked!!");
-   		super.onResume();
-   	}
-
-   	@Override
-   	protected void onPause() {
-   		logger.d(" onPause() invoked!!");
-   		super.onPause();
-   	}
-
-   	@Override
-   	protected void onStop() {
-   		logger.d(" onStop() invoked!!");
-   		super.onStop();
-   	}
-
-   	@Override
-   	public void onDestroy() {
-   		logger.d(" onDestroy() invoked!!");
-   		super.onDestroy();
-
-   		if (mRunningTask != null && mRunningTask.isCancelled() == false) {
-   			mRunningTask.cancel(false);
-   			mRunningTask = null;
-   		}
-   		if (mAlertDialog != null) {
-   			mAlertDialog.dismiss();
-   			mAlertDialog = null;
-   		}
-   	}
-
-  	public void finishAnim() 	{
-   		super.finish();
-   		overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
-   	}
-
-   	public void finish()	{
-   		super.finish();
-   	}
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
